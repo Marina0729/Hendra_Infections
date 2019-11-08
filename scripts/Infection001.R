b8aabe2c84eab3e0b19e1f6db425f72a1d476975
Alexander <ale097@csiro.au>
2019-11-08 16:42:07 +1100

    pretty plot

--- scripts/Infection001.R
+++ scripts/Infection001.R
@@ -4,12 +4,23 @@ read_csv("data/Hendra_Infection_001.csv")                                   #Rea
 TCID50 <- read_csv("data/Hendra_Infection_001.csv")
 TCID50
 
-TCID50 %>% 
+plot1 <- TCID50 %>% 
   filter(Lysis == "RIPA buffer") %>% 
   select(siRNA, time, Rep, TCID50) %>% 
   ggplot(mapping = aes(x = time, y = TCID50, shape = Rep, linetype = siRNA)) +
-  geom_line(size =) +
-  geom_point(size = 5) +
-  theme_bw()
+  geom_line(size = 1) +
+  geom_point(size = 3) +
+  theme_classic() +
+  theme(legend.text = element_text(size = 12), 
+        legend.title = element_text(size = 15),
+        axis.title = element_text(size = 15), 
+        axis.text.x = element_text(size = 12),
+        axis.title.y = element_text(size = 15),
+        axis.text.y = element_text(size = 12 ), 
+        legend.key.height = unit("0.5", "cm")) +
+  labs(title = "Hendra Infection 001 6 well format", x = "Time post infection (hrs)")
+  
 
+
+ggsave(filename = "results/TCID50_001_6well.png", plot = plot1)
 
